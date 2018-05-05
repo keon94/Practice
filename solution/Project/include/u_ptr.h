f257826ecc22ea37e939ca5948b6e02f98da46b6
Keon Amini <keonamini@yahoo.com>
2018-05-04 21:35:41 -0500

    PROJ-Utilities u_ptr implemented.

---
+++ solution/Project/include/u_ptr.h
@@ -0,0 +1,210 @@
+#ifndef U_PTR_H_
+#define U_PTR_H_
+
+#define _addr_(x) static_cast<void*>(&x)
+
+#ifndef PORT_DLL
+#define PORT_DLL
+#endif
+
+namespace util {
+
+    namespace {
+        template <typename T, typename U>
+        struct Types {
+            static constexpr bool ARE_SAME = false;
+        };
+
+        template <typename T>
+        struct Types<T, T> {
+            static constexpr bool ARE_SAME = true;
+        };
+
+        template<typename T>
+        struct IsArray {
+            static constexpr bool VALUE = false;
+        };
+
+        template<typename T>
+        struct IsArray<T[]> {
+            static constexpr bool VALUE = true;
+        };
+
+        template<typename T, unsigned N>
+        struct IsArray<T[N]> {
+            static constexpr bool VALUE = true;
+        };
+
+        template <typename T>
+        using Deleter = void(*)(T*);
+
+        template <typename T>
+        class b_ptr {
+
+        public:
+
+            virtual void setRaw(T* addr) = 0;
+
+            virtual T* getRaw() const = 0;
+
+        };
+    }
+
+    template <typename T>
+    class PORT_DLL u_ptr : public b_ptr<T>
+    {
+
+    public:
+
+        u_ptr() :
+            _deleter(setDefaultDeleter()),
+            _raw(nullptr)
+        {
+            if constexpr (IsArray<T>::VALUE && !Types<void, T>::ARE_SAME) {
+                _raw = new T();
+            }
+        }
+
+        u_ptr(T* ptr) :
+            _deleter(setDefaultDeleter()),
+            _raw(ptr) {}
+
+        template <typename ... Args> //new T(args...)
+        explicit u_ptr(Args&&... args) :
+            _deleter(setDefaultDeleter()),
+            _raw(new T(args...)) {}
+
+        ~u_ptr() {
+            clean<T>();
+        }
+
+        u_ptr(const u_ptr&) = delete;
+
+        u_ptr& operator= (const u_ptr& other) = delete;
+
+        template<typename U>
+        u_ptr(u_ptr<U>&& other) {
+            move<U>(other);
+        }
+
+        template<typename U>
+        u_ptr& operator= (U* other) {
+            move<U>(other);
+            return *this;
+        }
+
+        template<typename U>
+        u_ptr& operator= (u_ptr<U>&& other) {
+            move<U>(other);
+            return *this;
+        }
+
+        template<typename U>
+        U& operator* () {
+            return *_raw;
+        }
+
+        T* operator-> () {
+            return static_cast<T*>(_raw);
+        }
+
+        bool operator! () const {
+            return !_raw;
+        }
+
+        template<typename U>
+        operator U* () const {
+            return reinterpret_cast<U*>(_raw);
+        }
+
+        template<typename U>
+        bool operator== (const u_ptr<U>& other) const {
+            return _raw == other.get();
+        }
+
+        template<typename U>
+        bool operator== (const U* addr) const {
+            return _raw == addr;
+        }
+
+        template<typename U>
+        bool operator!= (const u_ptr<U>& other) const {
+            return _raw != other.get();
+        }
+
+        template<typename U>
+        bool operator!= (const U* addr) const {
+            return _raw != addr;
+        }
+
+        inline T* get() const {
+            return getRaw();
+        }
+
+        inline u_ptr& setDeleter(const Deleter<T>& deleter) {
+            _deleter = deleter;
+            return *this;
+        }
+
+        template <typename U>
+        inline Deleter<U> getDeleter() const {
+            if constexpr (!Types<U, T>::ARE_SAME)
+                return [](U* addr) { delete addr; };
+            else
+                return _deleter;
+        }
+
+    private:
+
+        template<typename U>
+        void clean() {
+            if (_raw) {
+                _deleter(static_cast<U*>(_raw));
+                _raw = nullptr;
+            }
+        }
+
+        template<typename U>
+        void move(u_ptr<U>& other) {
+            if (_addr_(*this) != _addr_(other)) {
+                clean<T>();
+                _raw = static_cast<b_ptr<U>&>(other).getRaw();
+                if constexpr (!Types<void, U>::ARE_SAME)
+                    _deleter = other.getDeleter<T>();
+                static_cast<b_ptr<U>&>(other).setRaw(nullptr);
+            }
+        }
+
+        template<typename U>
+        void move(U* addr) {
+            clean<T>();
+            _raw = addr;
+            if constexpr (!Types<void, U>::ARE_SAME)
+                _deleter = [](U* addr) { delete addr; };
+        }
+
+        virtual T* getRaw() const override {
+            return static_cast<T*>(_raw);
+        }
+
+        virtual void setRaw(T* addr) override {
+            _raw = addr;
+        }
+
+        inline Deleter<T> setDefaultDeleter() {
+            if constexpr (IsArray<T>::VALUE) {
+                return [](T* addr) { return delete[] addr; };
+            }
+            else {
+                return [](T* addr) { return delete addr; };
+            }
+        }
+
+    private:
+        void* _raw;
+        Deleter<T> _deleter;
+    };
+
+}
+#undef _addr_
+#endif
---
+++ solution/Tests/src/UniquePtrTests.cpp
@@ -0,0 +1,109 @@
+#include "gtest-utilities.h"
+#include "u_ptr.h"
+#include <vld.h>
+
+#define DISPLAY_LEAKS_DETAILS 1
+
+class UptrFixture : public testing::Test {
+
+protected:
+
+    struct X {
+        virtual int returnOne() = 0;
+        virtual ~X() {}
+    };
+
+    struct Y : public X {
+        char* str = new char[10];
+        ~Y() {
+            delete[] str;
+        }
+        virtual int returnOne() {
+            return 1;
+        }
+
+        int returnTwo() {
+            return 2;
+        }
+    };
+};
+
+using util::u_ptr;
+
+TEST_F(UptrFixture, void_to_int_moveAssignment) {
+    int x;
+    run([&]() {
+        u_ptr<void> ptr;
+        ptr = u_ptr<int>(7);
+        x = *static_cast<int*>(ptr.get());
+    });
+    ASSERT_EQ(x, 7);
+}
+
+TEST_F(UptrFixture, void_to_int_to_void_moveAssignment) {
+    int x;
+    run([&]() {
+        u_ptr<void> vptr1 = u_ptr<int>(7); 
+        u_ptr<int> intptr = std::move(vptr1);
+        u_ptr<void> vptr2 = std::move(intptr);
+        x = *static_cast<int*>(vptr2);
+    });
+    ASSERT_EQ(x, 7);
+}
+
+TEST_F(UptrFixture, void_to_int_moveConstructor) {
+    int x;
+    run([&]() {
+        u_ptr<void> ptr;
+        ptr = std::move(u_ptr<int>(7));
+        x = *static_cast<int*>(ptr.get());
+    });
+    ASSERT_EQ(x, 7);
+}
+
+TEST_F(UptrFixture, type_casts) {
+    run([&]() {
+        auto u = u_ptr<int>(5);
+        void* p_v = static_cast<void*>(u);
+        int* p_d = static_cast<int*>(p_v);
+        int x0 = *p_d;
+        _NO_LEAK_CHECK(ASSERT_EQ(x0, 5));
+    });
+}
+
+TEST_F(UptrFixture, arr_ptr) {
+    int x0, x1, x2;
+    run([&]() {
+        u_ptr<int> ptr = u_ptr<int[3]>();
+        ptr.get()[0] = 0;
+        ptr.get()[1] = 1;
+        ptr.get()[2] = 2;
+        int* p = ptr;
+        x0 = *p;
+        x1 = *++p;
+        x2 = *++p;
+    });
+    ASSERT_EQ(x0, 0);
+    ASSERT_EQ(x1, 1);
+    ASSERT_EQ(x2, 2);
+}
+
+TEST_F(UptrFixture, no_mem_leak_int_ptr) {
+    run([&]() {
+        int* x = new int(1);
+        {
+            u_ptr<int> ptr = x;
+        }
+    });
+}
+
+TEST_F(UptrFixture, polymorphism) {
+    run([&]() {
+        u_ptr<X> xPtr = u_ptr<Y>(new Y());
+        int x = xPtr->returnOne();
+        _NO_LEAK_CHECK(ASSERT_EQ(x, 1));
+        Y* yPtr = dynamic_cast<Y*>(xPtr.get());
+        int y = yPtr->returnTwo();
+        _NO_LEAK_CHECK(ASSERT_EQ(y, 2));
+    });
+}
